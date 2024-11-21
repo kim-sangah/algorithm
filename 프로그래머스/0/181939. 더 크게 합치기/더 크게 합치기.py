5cd6445289c12f69e9be8c0c25703a8a5ceb3c32
kim-sangah <kimsangah_@naver.com>
2024-11-21 11:38:48 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,8 +1,3 @@
 def solution(a, b):
-    if int(f"{a}{b}") > int(f"{b}{a}"):
-        answer = int(f"{a}{b}")
-    elif int(f"{a}{b}") == int(f"{b}{a}"):
-        answer = int(f"{a}{b}")
-    else:
-        answer = int(f"{b}{a}") 
+    answer = max(int(f"{a}{b}"), int(f"{b}{a}"))
     return answer
