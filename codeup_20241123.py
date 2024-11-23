9ae2a6b9ec7b1f326cbf87e2b3c574a8b3233390
kim-sangah <kimsangah_@naver.com>
2024-11-23 13:04:50 +0900

    Create codeup_20241123.py

---
+++ codeup_20241123.py
@@ -0,0 +1,45 @@
+6016
+a, b = input().split()
+print(b, a)
+
+6017
+s = input()
+print(s, s, s)
+
+6018
+a, b = input().split(":")
+print(a, b, sep=":")
+
+6019
+y,m,d = input().split(".")
+print(d,m,y,sep="-")
+
+6020
+a, b = input().split("-")
+print(a, b, sep="")
+
+6021
+a = input()
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+
+6022
+a = input()
+print(a[:2],a[2:4],a[4:],sep=" ")
+
+6023
+a,b,c = input().split(":")
+print(b)
+
+6024
+a, b = input().split()
+s = a + b
+print(s)
+
+6025
+a, b = input().split()
+s = int(a) + int(b)
+print(s)
