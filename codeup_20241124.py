5c6c848e9d644685d39a2c064bf6110b86ea9aad
kim-sangah <kimsangah_@naver.com>
2024-11-24 21:16:04 +0900

    Create codeup_20241124.py

---
+++ codeup_20241124.py
@@ -0,0 +1,49 @@
+6026
+a = input()
+b = input()
+s = float(a) + float(b)
+print(s)
+
+6027
+a = input()
+n = int(a)
+print('%x'%n)
+
+6028
+a = input()
+n = int(a)
+print('%X'%n)
+
+6029
+a = input()
+n = int(a, 16)
+print('%o'%n)
+
+6030
+a = ord(input())
+print(a)
+
+6031
+c = int(input())
+print(chr(c))
+
+6032
+n = int(input())
+print(-n)
+
+6033
+a = input()
+n = ord(a)
+print(chr(n+1))
+
+6034
+a, b = input().split()
+c = int(a) - int(b)
+print(c)
+
+6035
+a, b = input().split()
+f1 = float(a)
+f2 = float(b)
+m = f1 * f2
+print(m)
