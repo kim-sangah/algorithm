4492f2ba08c622ab906010e1a0262e44d65c7009
kim-sangah <kimsangah_@naver.com>
2024-11-25 15:06:20 +0900

    Create codeup_20241125.py

---
+++ codeup_20241125.py
@@ -0,0 +1,55 @@
+6036
+a, b = input().split()
+print(a*int(b))
+
+6037
+n = input()
+s = input()
+print(s*int(n))
+
+6038
+a, b = input().split()
+print(int(a)**int(b))
+
+6039
+a, b = input().split()
+f1 = float(a)
+f2 = float(b)
+print(f1**f2)
+
+6040
+a, b = input().split()
+print(int(a)//int(b))
+
+6041
+a, b = input().split()
+print(int(a)%int(b))
+
+6042
+a = float(input())
+print(format(a,".2f"))
+
+6043
+a, b = input().split()
+f1 = float(a)
+f2 = float(b)
+print(format(f1/f2,".3f"))
+
+6044
+ai, bi = input().split()
+a = int(ai)
+b = int(bi)
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+print(format(a/b, ".2f"))
+
+6045
+ai, bi, ci = input().split()
+a = int(ai)
+b = int(bi)
+c = int(ci)
+s = a+b+c
+print(s, format(s/3, ".2f"))
