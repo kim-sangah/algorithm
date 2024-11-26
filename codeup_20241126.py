5050f5289c80e8243c985d34ac08c57e8fef1a45
kim-sangah <kimsangah_@naver.com>
2024-11-26 11:49:10 +0900

    Create codeup_20241126.py

---
+++ codeup_20241126.py
@@ -0,0 +1,41 @@
+6046
+a = int(input())
+print(a<<1)
+
+6047
+a1, b1 = input().split()
+a = int(a1)
+b = int(b1)
+print(a<<b)
+
+6048
+a, b = input().split()
+print(int(a)<int(b))
+
+6049
+a, b = input().split()
+print(int(a)==int(b))
+
+6050
+a, b = input().split()
+print(int(a)<=int(b))
+
+6051
+a, b = input().split()
+print(int(a)!=int(b))
+
+6052
+a = int(input())
+print(bool(a))
+
+6053
+a = bool(int(input()))
+print(not a)
+
+6054
+a, b = input().split()
+print(bool(int(a)) and bool(int(b)))
+
+6055
+a, b = input().split()
+print(bool(int(a)) or bool(int(b)))
