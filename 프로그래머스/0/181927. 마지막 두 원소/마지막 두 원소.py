a91f7fff6e980862112d61b7620ae8e700f269e6
kim-sangah <kimsangah_@naver.com>
2024-12-27 10:35:33 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -0,0 +1,9 @@
+def solution(num_list):
+    answer = num_list
+    a = num_list[-1]
+    b = num_list[-2]
+    if a > b:
+        answer.append(a-b)
+    else:
+        answer.append(a*2)
+    return answer
