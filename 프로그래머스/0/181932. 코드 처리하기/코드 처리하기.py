cd656ac7ff8c8379fe407d4aa34da9778231030c
kim-sangah <kimsangah_@naver.com>
2025-01-02 21:37:01 +0900

    [level 0] Title: 코드 처리하기, Time: 13.34 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -3,15 +3,15 @@ def solution(code):
     mode = 0
     for i in range(len(code)):
         if mode==0:
-            if i%2==0 and code[i]!='1':
-                answer += code[i]
-            elif code[i]=='1':
+            if code[i]=='1':
                 mode = mode + 1
-        elif mode==1:
-            if i%2==1 and code[i]!='1':
+            elif i%2==0:
                 answer += code[i]
-            elif code[i]=='1':
+        else:
+            if code[i]=='1':
                 mode = mode - 1
+            elif i%2==1:
+                answer += code[i]
     if not answer:
         answer = "EMPTY"
     return answer
