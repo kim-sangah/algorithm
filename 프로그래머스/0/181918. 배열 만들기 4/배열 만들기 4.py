f776fc281ae6960b33e37e7380f453206cf4acbc
kim-sangah <kimsangah_@naver.com>
2025-01-06 16:31:15 +0900

    [level 0] Title: 배열 만들기 4, Time: 45.68 ms, Memory: 13.9 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
@@ -2,12 +2,9 @@ def solution(arr):
     stk = []
     i = 0
     while i < len(arr):
-        if not stk:
+        if not stk or stk[-1] < arr[i]:
             stk.append(arr[i])
-            i = i + 1
-        elif stk[-1] < arr[i]:
-            stk.append(arr[i])
-            i = i + 1
-        elif stk[-1] >= arr[i]:
+            i += 1
+        else:
             stk.pop()
     return stk
