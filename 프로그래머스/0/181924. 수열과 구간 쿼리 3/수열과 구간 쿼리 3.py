cd1b7b4ed9356b7c8455080c612f0374da1e363e
kim-sangah <kimsangah_@naver.com>
2025-01-07 14:05:40 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.08 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -1,6 +1,4 @@
 def solution(arr, queries):
-    for i in range(len(queries)):
-        a = queries[i][0]
-        b = queries[i][1]
+    for a, b in queries:
         arr[a], arr[b] = arr[b], arr[a]
     return arr
