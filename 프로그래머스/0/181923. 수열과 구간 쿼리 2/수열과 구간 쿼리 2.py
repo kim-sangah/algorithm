a9fbb9e517d7d40d475a6a8a1fe9476445537506
kim-sangah <kimsangah_@naver.com>
2025-01-07 14:59:02 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 7.98 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -2,8 +2,5 @@ def solution(arr, queries):
     answer = []
     for s, e, k in queries:
         l = [num for num in arr[s:e+1] if num > k]
-        if l:
-            answer.append(min(l))
-        else:
-            answer.append(-1)
+        answer.append(min(l) if l else -1)
     return answer
