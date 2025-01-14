164d9b078d44509741c38271d82db98d7cf93d2b
kim-sangah <kimsangah_@naver.com>
2025-01-14 10:05:37 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -1,8 +1,5 @@
 def solution(my_string, is_suffix):
     answer = 0
-    my_list = []
-    for i in range(len(my_string)):
-        my_list.append(my_string[i:])
-    if is_suffix in my_list:
+    if is_suffix == my_string[-len(is_suffix):]:
         answer += 1
     return answer
