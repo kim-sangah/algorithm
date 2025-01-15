75c2bbda865cdfbcc12d7229db669a44d85f65d1
kim-sangah <kimsangah_@naver.com>
2025-01-15 10:19:46 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.26 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -1,9 +1,6 @@
 def solution(my_string):
-    answer = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    ABC_list = [chr(i) for i in range(65, 91)]
-    abc_list = [chr(i) for i in range(97, 123)]
-    chr_list = ABC_list + abc_list
+    answer = [0] * 52
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     for string in my_string:
-        i = chr_list.index(string)
-        answer[i] += 1
+        answer[alphabet.index(string)] += 1
     return answer
