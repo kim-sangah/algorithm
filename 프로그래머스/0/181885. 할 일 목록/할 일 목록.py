6155f9b55e00ae9ed5e3b110f5b6d5ab1b1401b8
kim-sangah <kimsangah_@naver.com>
2025-01-22 17:19:55 +0900

    [level 0] Title: 할 일 목록, Time: 0.01 ms, Memory: 9.88 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
@@ -1,6 +1,6 @@
 def solution(todo_list, finished):
     answer = []
     for i, w in enumerate(todo_list):
-        if not finished[i]:
+        if finished[i] == False:
             answer.append(w)
     return answer
