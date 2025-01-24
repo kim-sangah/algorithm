cc8c58bdeeada919b21ea6e4e66948b2b7a42f8f
kim-sangah <kimsangah_@naver.com>
2025-01-24 23:35:42 +0900

    [level 0] Title: A 강조하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.py
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.py
@@ -1,9 +1,2 @@
 def solution(myString):
-    myString = myString.lower()
-    answer = ''
-    for i in myString:
-        if i == "a":
-            answer += "A"
-        else:
-            answer += i
-    return answer
+    return myString.lower().replace("a", "A")
