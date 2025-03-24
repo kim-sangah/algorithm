e447d281a3f399bc4dbb18499b4552f92b939294
kim-sangah <kimsangah_@naver.com>
2025-03-24 10:25:46 +0900

    [level 0] Title: 커피 심부름, Time: 0.13 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
@@ -1,8 +1,6 @@
 def solution(order):
     answer = 0
     for i in order:
-        if "cafelatte" in i:
-            answer += 5000
-        else:
-            answer += 4500
+        if "latte" in i: answer += 5000 
+        else: answer += 4500
     return answer
