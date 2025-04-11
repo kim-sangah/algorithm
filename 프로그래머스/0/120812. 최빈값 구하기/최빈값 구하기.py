0165b73924e6e183ccde16f3079b60d103928cca
sangah <kimsangah_@naver.com>
2025-04-11 23:25:50 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.08 ms, Memory: 9.06 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,7 +1,10 @@
 def solution(array):
     answer = [0] * 1000
+    
     for num in array:
         answer[num] += 1
-    if answer.count(max(answer)) > 1:
+        
+    max_num = max(answer)
+    if answer.count(max_num) > 1:
         return -1
-    return answer.index(max(answer))
+    return answer.index(max_num)
