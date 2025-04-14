c1f98ca76bccbca50babd30f44c4273f3a4e410f
sangah <kimsangah_@naver.com>
2025-04-14 20:43:37 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,10 +1,3 @@
 def solution(price):
-    discount = [
-        (10, 99999, 1),
-        (100000, 299999, 0.95),
-        (300000, 499999, 0.9),
-        (500000, 1000000, 0.8)
-    ]
-    for low, high, rate in discount:
-        if low <= price <= high:
-            return int(price*rate)
+    rate = [1, 0.95, 0.95, 0.9, 0.9, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8]
+    return int(price*rate[price//100000])
