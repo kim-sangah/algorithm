6a754c6c6f58464e752a08ea5fa2101ccbfff445
sangah <kimsangah_@naver.com>
2025-04-22 19:52:16 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -2,5 +2,5 @@ def solution(numbers, direction):
     if direction == 'right':
         answer = numbers[-1:] + numbers[:len(numbers)-1]
     else:
-        answer = numbers[1:] + numbers[0:1]
+        answer = numbers[1:] + numbers[:1]
     return answer
