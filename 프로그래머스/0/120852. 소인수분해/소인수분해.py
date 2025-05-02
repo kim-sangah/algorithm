41f2b6c03ae324b064f608a1f051bf2bd19d02f3
sangah <kimsangah_@naver.com>
2025-05-02 11:04:01 +0900

    [level 0] Title: 소인수분해, Time: 1.03 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.py
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -1,15 +1,11 @@
 def solution(n):
     answer = []
     i = 2
-    
     while i <= n:
-        if n % i:
-            i += 1
-        else:
+        if n % i == 0:
             n //= i
             if i not in answer:
                 answer.append(i)
-    if n > 1:
-        answer.append(n)
-        
+        else:
+            i += 1 
     return answer
