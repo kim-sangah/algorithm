92c30f3f410977e61228cc38c4ebd2ac69a9adc1
sangah <kimsangah_@naver.com>
2025-05-03 19:47:43 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.17 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -1,8 +1,2 @@
 def solution(my_string):
-    answer = ''
-    for i in my_string:
-        if i.isupper():
-            answer += i.lower()
-        else:
-            answer += i.upper()
-    return answer
+    return ''.join(i.lower() if i.isupper() else i.upper() for i in my_string)
