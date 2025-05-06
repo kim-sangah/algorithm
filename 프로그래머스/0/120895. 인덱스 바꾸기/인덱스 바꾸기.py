41b2ceece3cb6f15bb12268f31db8551442f1bbe
sangah <kimsangah_@naver.com>
2025-05-06 17:55:00 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -1,10 +1,4 @@
 def solution(my_string, num1, num2):
-    answer = ''
-    for i in range(len(my_string)):
-        if i == num1:
-            answer += my_string[num2]
-        elif i == num2:
-            answer += my_string[num1]
-        else:
-            answer += my_string[i]
-    return answer
+    m = list(my_string)
+    m[num1], m[num2] = m[num2], m[num1]
+    return ''.join(m)
