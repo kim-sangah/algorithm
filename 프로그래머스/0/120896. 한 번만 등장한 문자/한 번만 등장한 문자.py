facbc35f6b18eae40320f4b42843d8c1e43f43c9
sangah <kimsangah_@naver.com>
2025-05-07 11:51:06 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.02 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -1,8 +1,2 @@
 def solution(s):
-    d = {}
-    for k in s:
-        if k in d:
-            d[k] += 1
-        else:
-            d[k] = 1
-    return ''.join(sorted(k for k, v in d.items() if v==1))
+    return ''.join(sorted(ch for ch in s if s.count(ch)==1))
