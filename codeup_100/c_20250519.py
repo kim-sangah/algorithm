ef3bc6c17248277e078c67d1ca079877187098c1
sangah <kimsangah_@naver.com>
2025-05-19 18:02:39 +0900

    Create c_20250519.py

---
+++ codeup_100/c_20250519.py
@@ -0,0 +1,28 @@
+1003
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello\nWorld");
+    return 0;
+}
+
+
+1004
+#include <stdio.h>
+
+int main()
+{
+    printf("\'Hello\'");
+    return 0;
+}
+
+
+1005
+#include <stdio.h>
+
+int main()
+{
+    printf("\"Hello World\"");
+    return 0;
+}
