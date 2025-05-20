3d006a04bb9055349688f96bb82439a9cae29b77
sangah <kimsangah_@naver.com>
2025-05-21 00:07:50 +0900

    Create c_20250521.py

---
+++ codeup_100/c_20250521.py
@@ -0,0 +1,30 @@
+1006
+#include <stdio.h>
+
+int main()
+{
+    printf("\"!@#$%%^&*()\"\n");
+    return 0;
+}
+
+
+1007
+#include <stdio.h>
+
+int main()
+{
+    printf("\"C:\\Download\\hello.cpp\"");
+    return 0;
+}
+
+
+1008
+#include <stdio.h>
+
+int main()
+{
+    printf("\u250C\u252C\u2510\n");
+    printf("\u251C\u253C\u2524\n");
+    printf("\u2514\u2534\u2518\n");
+    return 0;
+}
