25025942eca0b569cbc05c5ead359ac464121ca6
sangah <kimsangah_@naver.com>
2025-05-23 09:58:00 +0900

    Create c_20250523.py

---
+++ codeup_100/c_20250523.py
@@ -0,0 +1,34 @@
+1013
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d%d", &a, &b);
+    printf("%d %d", a, b);
+    return 0;
+}
+
+
+1014
+#include <stdio.h>
+
+int main()
+{
+    char a, b;
+    scanf("%c %c", &a, &b);
+    printf("%c %c", b, a);
+    return 0;
+}
+
+
+1015
+#include <stdio.h>
+
+int main()
+{
+    float a;
+    scanf("%f", &a);
+    printf("%.2f", a);
+    return 0;
+}
