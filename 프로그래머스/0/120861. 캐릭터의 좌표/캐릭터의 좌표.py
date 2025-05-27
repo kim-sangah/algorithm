07cf45d9fe27dd7f03c77b5eb7c5c5b01a7a7226
sangah <kimsangah_@naver.com>
2025-05-27 12:50:20 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.02 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -1,12 +1,13 @@
 def solution(keyinput, board):
     key_dict = {'up':[0,1], 'down':[0,-1], 'left':[-1,0], 'right':[1,0]}
+    x_lim, y_lim = board[0]//2, board[1]//2
     answer = [0,0]
     
     for k in keyinput:
         x, y = key_dict.get(k, [0,0])
-        if -(board[0]//2) <= answer[0] + x <= board[0]//2:
+        if abs(answer[0] + x) <= x_lim:
             answer[0] += x
-        if -(board[1]//2) <= answer[1] + y <= board[1]//2:
+        if abs(answer[1] + y) <= y_lim:
             answer[1] += y
         
     return answer
