d4c8bad896b52447d0c23036c34e819840e27e00
sangah <kimsangah_@naver.com>
2025-05-31 10:50:16 +0900

    Create c_20250531.py

---
+++ codeup_100/c_20250531.py
@@ -0,0 +1,34 @@
+1028
+#include <stdio.h>
+
+int main()
+{
+    unsigned int n;
+    scanf("%u", &n);
+    printf("%u", n);
+    return 0;
+}
+
+
+1029
+#include <stdio.h>
+
+int main()
+{
+    double d;
+    scanf("%lf", &d);
+    printf("%.11lf", d);
+    return 0;
+}
+
+
+1030
+#include <stdio.h>
+
+int main()
+{
+    long long int n;
+    scanf("%lld", &n);
+    printf("%lld", n);
+    return 0;
+}
