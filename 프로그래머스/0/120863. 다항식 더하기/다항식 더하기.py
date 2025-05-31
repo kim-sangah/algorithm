13f0bf04cdb44f9cf9957e22e1cd05e687dff3bf
sangah <kimsangah_@naver.com>
2025-05-31 11:39:35 +0900

    [level 0] Title: 다항식 더하기, Time: 0.03 ms, Memory: 9.49 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.py
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.py
@@ -1,24 +1,18 @@
 def solution(polynomial):
     terms = polynomial.split(' + ')
-    
     x_sum = 0
     num_sum = 0
-     
+    
     for term in terms:
-        if 'x' in term:
-            if term == 'x':
-                x_sum += 1
-            else:
-                x_sum += int(term.replace('x', ''))
-        else:
+        if term.isdigit():
             num_sum += int(term)
+        else:
+            x_sum += 1 if term == 'x' else int(term[:-1])
     
     answer = []
-    if x_sum == 1:
-        answer.append('x')
-    elif x_sum > 1:
-        answer.append(f'{x_sum}x')
-    if num_sum > 0:
+    if x_sum:
+        answer.append('x' if x_sum == 1 else f'{x_sum}x')
+    if num_sum:
         answer.append(f'{num_sum}')
         
     return ' + '.join(answer)
