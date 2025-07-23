ffb94377bdf83a9a64ccc1fefb1179b6d226be8a
sangah <kimsangah_@naver.com>
2025-07-23 20:01:51 +0900

    Create c_20250723.c

---
+++ codeup_100/c_20250723.c
@@ -0,0 +1,22 @@
+1034
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%o", &n);
+    printf("%d", n);
+    return 0;
+}
+
+
+1035
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%x", &n);
+    printf("%o", n);
+    return 0;
+}
