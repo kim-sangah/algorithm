d3ea4f96e1f87680f9b1593d45a60b52e1bac848
sangah <kimsangah_@naver.com>
2025-07-28 22:06:29 +0900

    Create c_20250728.c

---
+++ codeup_100/c_20250728.c
@@ -0,0 +1,58 @@
+1036
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%c", &n);
+    printf("%d", n);
+    return 0;
+}
+
+
+1037
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    printf("%c", n);
+    return 0;
+}
+
+
+1038
+#include <stdio.h>
+
+int main()
+{
+    long long int a, b;
+    scanf("%lld %lld", &a, &b);
+    printf("%lld", a+b);
+    return 0;
+}
+
+
+1039
+#include <stdio.h>
+
+int main()
+{
+    long long int a, b;
+    scanf("%lld %lld", &a, &b);
+    printf("%lld", a+b);
+    return 0;
+}
+
+
+1040
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    printf("%d", -a);
+    return 0;
+}
