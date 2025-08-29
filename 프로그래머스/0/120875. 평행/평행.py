6cd4161d57492682c3e38b207cfe9a5e0fbb04ef
sangah <kimsangah_@naver.com>
2025-08-29 22:01:33 +0900

    [level 0] Title: 평행, Time: 0.02 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.py
+++ 프로그래머스/0/120875. 평행/평행.py
@@ -1,15 +1,16 @@
 def solution(dots):
-    a, b, c, d = dots
-    
     case = [
-        (a, b, c, d),
-        (a, c, b, d),
-        (a, d, b, c)
+        (0, 1, 2, 3),
+        (0, 2, 1, 3),
+        (0, 3, 1, 2)
     ]
     
     for d1, d2, d3, d4 in case:
-        dx1, dy1 = d1[0] - d2[0], d1[1] - d2[1]
-        dx2, dy2 = d3[0] - d4[0], d3[1] - d4[1]
-        if dy1*dx2 == dy2*dx1:
+        x1, y1 = dots[d1]
+        x2, y2 = dots[d2]
+        x3, y3 = dots[d3]
+        x4, y4 = dots[d4]
+        
+        if (y1-y2)*(x3-x4) == (y3-y4)*(x1-x2):
             return 1
     return 0
