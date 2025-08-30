23c973042b179a70e45a9c16876fda9398cc6982
sangah <kimsangah_@naver.com>
2025-08-30 21:27:39 +0900

    [level 0] Title: 문자열 밀기, Time: 0.01 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
@@ -1,10 +1,8 @@
 def solution(A, B):
-    answer = 0
 
     for i in range(len(A)):
         if A == B:
-            return answer
+            return i
         A = A[-1] + A[:-1]
-        answer += 1
 
     return -1
